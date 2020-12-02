926991182dd99a731e97fe7af2938a966d7ed101
onias17 <oniasnephiisrael@gmail.com>
2020-12-01 16:27:26 -0800

    created index model

--- models/index.js
+++ models/index.js
@@ -1,1 +1,20 @@
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected succesfully...');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+});
+
+module.exports = {
+  Appointment: require('./Appointment'),
+};
